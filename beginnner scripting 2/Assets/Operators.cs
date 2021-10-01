ab0fa7dd048ec36925314cf88b2a882ca1c1e97c
Mathew Harris <10959101@uvu.edu>
2021-10-01 13:55:04 -0600

    Created Code for variables and operators
    After completion of the example codes in the module, I defined my own variables and operators in my Unity project.

---
+++ beginnner scripting 2/Assets/Operators.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Operators : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myNum = 1;
+        int Nummy = 2;
+        int Num = 3;
+
+        int sum1 = myNum + Nummy;
+        int sum2 = Nummy + Num;
+        int sum3 = Num + myNum;
+        int sum4 = myNum + Num + Nummy;
+
+        int sum5 = ++myNum;
+        int sum4 = --myNum;
+
+
+
+        
+    }
+
+   
+}
---
+++ beginnner scripting 2/Assets/Variables.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myNum = 1;
+        int Nummy = 2;
+
+        string hello = "hello";
+        string bye = "bye";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
